06b18c79129f7862006af725500705e2b4b44fb0
danny <danny.eeraerts@proximus.be>
2020-03-21 12:32:38 +0100

    Add names to routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,16 +10,15 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('OMG! My first page already! WOO0!');
+        return $this->render('article/homepage.html.twig');
     }
 
-
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
